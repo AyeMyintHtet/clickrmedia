be195626f307a6655abe0f1639a4f2654d205514
AyeMyintHtet <ayemyinthtet099@gmail.com>
2022-11-27 18:32:21 +0630

    update

--- routes/auth.js
+++ routes/auth.js
@@ -21,10 +21,10 @@ router.post('/register', async (req,res)=>{
     
     // console.log('req.body.userrole', req.body.userrole);
     const {error} =await registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+    if(error) return res.status(400).json(error.details[0].message);
     //check user already in database
     const emailExist = await User.findOne({email : req.body.email});
-    if(emailExist) return res.status(400).send('Email alredy exist');
+    if(emailExist) return res.status(400).json({message:'Email alredy exist'});
     // Hash the password 
     const  salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(req.body.password,salt);
@@ -50,13 +50,13 @@ router.post('/register', async (req,res)=>{
 
 router.post('/login',async (req,res)=>{
     const {error} =await loginValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+    if(error) return res.status(400).json(error.details[0].message);
     //check mail
     const user = await User.findOne({email : req.body.email});
-    if(!user) return res.status(400).send('Email or Password was wrong');
+    if(!user) return res.status(400).json('Email or Password was wrong');
     //password correct 
     const validPass = await bcrypt.compare(req.body.password,user.password);
-    if(!validPass) return res.status(400).send('Email or Password was wrong');
+    if(!validPass) return res.status(400).json('Email or Password was wrong');
     //create assign token 
     const token = jwt.sign({_id : user._id}, process.env.TOKEN_SEC);
     res.header('auth-token',token).json({'auth-token':token,user,'status':'success'});
